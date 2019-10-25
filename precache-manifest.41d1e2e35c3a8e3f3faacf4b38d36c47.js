3d7ef5ebc50a1fcc974a6322df42dfe4986a8648
Justin Steenhuis <justinsteenhuis@gmail.com>
2019-10-26 00:36:09 +0200

    Updates

---
+++ precache-manifest.41d1e2e35c3a8e3f3faacf4b38d36c47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75c798c766e6decdadf174c34c170065",
+    "url": "/extent/index.html"
+  },
+  {
+    "revision": "81395a4ab09513e141c2",
+    "url": "/extent/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "ffd4bc142d49c7f46e96",
+    "url": "/extent/static/css/main.ef81d212.chunk.css"
+  },
+  {
+    "revision": "81395a4ab09513e141c2",
+    "url": "/extent/static/js/2.d1709a12.chunk.js"
+  },
+  {
+    "revision": "ffd4bc142d49c7f46e96",
+    "url": "/extent/static/js/main.78a71951.chunk.js"
+  },
+  {
+    "revision": "d2f3408092290c5b86a1",
+    "url": "/extent/static/js/runtime-main.360c7584.js"
+  }
+]);
