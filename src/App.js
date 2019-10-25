c4ef890f1287f457b717c636036fc6c4397ab496
Justin Steenhuis <justinsteenhuis@gmail.com>
2019-10-26 00:29:53 +0200

    Make extent

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Map, TileLayer, GeoJSON } from "react-leaflet";
+import shp from "shpjs";
+import concaveman from "concaveman";
+
+import "leaflet/dist/leaflet.css";
 
 function App() {
+  const [points, setPoints] = useState();
+  const [polygon, setPolygon] = useState();
+
+  const handleFile = file2 => {
+    var reader = new FileReader();
+
+    reader.addEventListener(
+      "load",
+      () => {
+        const geoJson = shp.parseShp(reader.result);
+        setPoints(geoJson);
+        const points = geoJson.map(item => item.coordinates);
+        const polygon = concaveman(points);
+        const geoJsonPolygon = {
+          type: "Feature",
+          geometry: {
+            type: "Polygon",
+            coordinates: [polygon]
+          }
+        };
+        setPolygon(geoJsonPolygon);
+      },
+      false
+    );
+    if (file2.target.files) {
+      reader.readAsArrayBuffer(file2.target.files[0]);
+    }
+  };
+
+  let position = [51.84276079, 5.18380148];
+  if (polygon) {
+    const [first, second] = polygon.geometry.coordinates[0][0];
+    position = [second, first];
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="map">
+      <Map center={position} zoom={15} className="leaflet-map">
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {/* {points && <GeoJSON data={points} />} */}
+        {polygon && <GeoJSON data={polygon} />}
+      </Map>
+      <input type="file" id="input" onChange={handleFile}></input>
     </div>
   );
 }
