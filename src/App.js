9e799bfdc2a9d9474d107a1910230f018331ef19
Justin Steenhuis <justinsteenhuis@gmail.com>
2019-10-26 12:06:09 +0200

    Layers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
 import "./App.css";
-import { Map, TileLayer, GeoJSON } from "react-leaflet";
+import { Map, TileLayer, GeoJSON, LayersControl } from "react-leaflet";
 import shp from "shpjs";
 import concaveman from "concaveman";
+import L from "leaflet";
 
 import "leaflet/dist/leaflet.css";
+
+const { BaseLayer, Overlay } = LayersControl;
 
 function App() {
   const [points, setPoints] = useState();
   const [polygon, setPolygon] = useState();
+
+  const pointToLayer = (feature, latLng) => {
+    return L.circleMarker(latLng, {
+      radius: 2,
+      fillColor: "#ff7800",
+      color: "#ff0000"
+    });
+  };
 
   const handleFile = file2 => {
     var reader = new FileReader();
@@ -49,8 +60,18 @@ function App() {
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {/* {points && <GeoJSON data={points} />} */}
-        {polygon && <GeoJSON data={polygon} />}
+        <LayersControl position="topright">
+          {polygon && (
+            <Overlay checked name="Polygon">
+              <GeoJSON data={polygon} />
+            </Overlay>
+          )}
+          {points && (
+            <Overlay name="Points">
+              <GeoJSON data={points} pointToLayer={pointToLayer} />
+            </Overlay>
+          )}
+        </LayersControl>
       </Map>
       <input type="file" id="input" onChange={handleFile}></input>
     </div>
